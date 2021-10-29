54d6ed5f5311900682ce7c85921873527527a30c
artkori <92971343+artkori@users.noreply.github.com>
2021-10-29 17:38:19 +0300

    Add files via upload

---
+++ lb 6.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int min, max;
+    float x, h, y;
+    cout << "Enter [min, max] and h\n";
+    cin >> min >> max >> h;
+    cout << "\tx" << "\t(x)" << endl;
+    x = min;
+    while (x <= max + h / 2)
+    {
+        y = ((x), 1 / 2) + 3 * x + x * x;
+        cout << setw(10) << x << setw(10) << setprecision(3) << y << endl;
+        x += h;
+    }
+    cout << "Do....While" << endl;
+    x = min;
+    cout << "\tx" << "\t(x)" << endl;
+    do
+    {
+        y = pow((x), 1 / 2) + 3 * x + x * x;
+        cout << setw(10) << x << setw(10) << setprecision(3) << y << endl;
+        x += h;
+    } while (x <= max + h / 2);
+}
